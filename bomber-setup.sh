ff5b4a3e65b723631ea99b4520bdaf02175c38ed
Pat Peters <Pat_Peters@gallup.com>
2022-12-16 14:38:16 -0600

    Creacted basic action YAML and shell script to install and run bomber

---
+++ bomber-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This script downloads the latest release of bomber, extracts the files out of the package
+# and runs bomber scan on the SBOM generated from the Syft action
+
+VERSION=0.4.0
+SBOM=$1
+
+echo "Setting Up Bomber"
+wget --progress=bar:force:noscroll "https://github.com/devops-kung-fu/bomber/releases/download/v${VERSION}/bomber_${VERSION}_linux_arm64.deb" && sudo dpkg --install "bomber_${VERSION}_linux_arm64.deb"
+
+# clean up remaining package
+rm "bomber_${VERSION}_linux_arm64.deb"
+echo "Running Bomber Scan on Generated SBOM"
+bomber scan bomber.cyclonedx.json --output=json > bomber_results.json
