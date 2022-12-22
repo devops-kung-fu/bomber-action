65765f14bbba4fc3d52f65d705481d99a54d1b6a
Pat Peters <Pat_Peters@gallup.com>
2022-12-22 14:34:24 -0600

    removed file generation that was possibly causing error

--- bomber-setup.sh
+++ bomber-setup.sh
@@ -10,4 +10,4 @@ wget --progress=bar:force:noscroll "https://github.com/devops-kung-fu/bomber/rel
 # clean up remaining package
 rm "bomber_${VERSION}_linux_amd64.deb"
 echo "Running Bomber Scan on Generated SBOM"
-bomber scan bomber.cyclonedx.json --output=json > bomber_results.json
+bomber scan bomber.cyclonedx.json
