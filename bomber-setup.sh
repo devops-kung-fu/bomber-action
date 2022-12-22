24897928a07df366265acabf44c89b0a4281f432
Pat Peters <Pat_Peters@gallup.com>
2022-12-22 14:16:29 -0600

    updated echo statement

--- bomber-setup.sh
+++ bomber-setup.sh
@@ -9,5 +9,5 @@ wget --progress=bar:force:noscroll "https://github.com/devops-kung-fu/bomber/rel
 
 # clean up remaining package
 rm "bomber_${VERSION}_linux_amd64.deb"
-echo "Running Bomber Scan on Generated SBOM"
+echo "Running Bomber Scan on Generated SBOM file"
 bomber scan bomber.cyclonedx.json --output=json > bomber_results.json
